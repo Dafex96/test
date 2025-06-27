db7b87328244569ffe1d24d15b090d18772e89d2
David <davicolo717@gmail.com>
2025-06-27 13:03:56 -0400

    test final

--- funciones.py
+++ funciones.py
@@ -9,19 +9,38 @@ def menu():
 
 def agregar():
     
-    nombre = input("Ingrese el nombe se usuario: ").lower()
+    nombre = input("Ingrese el nombre se usuario: ").lower()
     sexo = input("Ingrese el sexo (M:Masculino), (F:Femenino): ").upper()
     
-    while sexo == "M" and sexo == "F":
-        break
-    else:
-        print("Debe ingresar 'M' para Masculino y 'F' para Femenino.")
-        sexo = input("Ingrese el sexo: ").upper()
+    while True:
+        if sexo == "M" or sexo == "F":
+            break
+        else:
+            print("Debe ingresar solamente 'M' para masculino y 'F' para Femenino")
+            sexo = input("Ingrese el sexo (M:Masculino), (F:Femenino): ").upper()
     
-    contra = input("Ingrese la contraseña: ")
-
-    usuarios[nombre] = {"nombre": nombre,"sexo": sexo,"contraseña":contra}
-    print("¡Usuario añadido!")
+    contra = input("Ingrese la contraseña: ").lower()
+    if len(contra) < 8:
+        print("La contraseña debe tener minimo 8 caracteres")
+        contra = input("Ingrese la contraseña: ")
+    if "1"or"2"or"3"or"4"or"5"or"6"or"7"or"8"or"9"or"0" not in contra:
+        print("La contraseña debe tener al menos un numero")
+        contra = input("Ingrese la contraseña: ")
+    
+    letra = False
+    
+    for l in contra:
+        if 'a' <= l.lower() <= 'z':
+            letra = True
+        if not letra:
+            print("La contraseña debe tener al menos una  letra")
+        contra = input("Ingrese la contraseña: ")
+        if ' ' in contra:
+            print("La contraseña no debe tener espacios")
+            contra = input("Ingrese la contraseña: ")
+        usuarios[nombre] = {"nombre": nombre,"sexo": sexo,"contraseña":contra}
+        print("¡Usuario añadido!")
+        return False
 
 def buscar():
     nombre = input("Inserte el nombre de usuario a buscar: ").lower()
