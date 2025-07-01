ca7082af8e93fed527525bcf9b693cee4f0f8953
David <davicolo717@gmail.com>
2025-07-01 12:50:05 -0400

    testing

---
+++ draft.py
@@ -0,0 +1,25 @@
+#albums = {album_id: [title, artist, genre, release_year,format_]}
+
+albums = {
+    'ALB001': ['A Night at the Opera', 'Queen', 'Rock', 1975, 'Vinyl'],
+    'ALB002': ['Thriller', 'Michael Jackson', 'Pop', 1982, 'CD'],
+    'ALB003': ['Dark Side of the Moon', 'Pink Floyd', 'Rock', 1973, 'Vinyl'],
+    'ALB004': ['21', 'Adele', 'Pop', 2011, 'Digital'],
+    'ALB005': ['Back in Black', 'AC/DC', 'Rock', 1980, 'CD'],
+    'ALB006': ['Abbey Road', 'The Beatles', 'Rock', 1980, 'CD'],
+    'ALB007': ['GNX', 'Kendrick Lamar', 'Rap', 2024, 'Digital'],
+    'ALB008': ['Make it Big', 'Wham!', 'Pop', 1984, 'Vinyl'],
+}
+
+#inventory = {album_id: [price, stock]}
+
+inventory = {
+    'ALN001': [6],
+    'ALN002': [7],
+    'ALN003': [8],
+    'ALN004': [9],
+    'ALN005': [10],
+    'ALN006': [0],
+    'ALN007': [11],
+    'ALN008': [12],
+}
